fd38c6d24da115e3903fedef3180143b16265dff
Aviel <aviell1100611@gmail.com>
2024-01-04 04:01:36 +0200

    First upload of virtual world client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import playerImage from "./player.png";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3001");
 
 function App() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    // Emit newPlayer event when the component mounts
+    socket.emit("newPlayer", { id: socket.id, x: 0, y: 0 });
+
+    const updatePlayersListener = (playerNewPositions) => {
+      setPlayers(playerNewPositions);
+    };
+    socket.on("updatePlayers", updatePlayersListener);
+
+    return () => {
+      socket.off("updatePlayers", updatePlayersListener);
+    };
+  }, []);
+
+  const handleMouseClick = (event) => {
+    const offsetXAdjustment = -65; // Adjust as needed
+    const offsetYAdjustment = -200; // Adjust as needed
+
+    const mouseX = event.clientX + offsetXAdjustment;
+    const mouseY = event.clientY + offsetYAdjustment;
+
+    // Emit the new player position to the server
+    socket.emit("playerMoved", { id: socket.id, x: mouseX, y: mouseY });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="world" onClick={handleMouseClick}>
+        {players.map((player) => (
+          <img
+            key={player.id}
+            src={playerImage}
+            alt={`Player ${player.id}`}
+            className="character"
+            style={{
+              transform: `translate(${player.x}px, ${player.y}px)`,
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
